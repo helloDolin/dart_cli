6044ca8ac10bad27c2e1c88cc19116045647d407
Dolin Liao <dolin.liao@banding.cn>
2024-10-22 16:12:26 +0800

    fix：eg

--- bin/help_text.dart
+++ bin/help_text.dart
@@ -4,12 +4,12 @@ A hotdog command-line tool powered by shaolin.
 
 Usage: hd <command> [arguments]
 
-eg:
-hd create:page friends_list on friends
-hd create:request on friends
+Eg:
+hd create page:friends_list on friends
+hd create request on friends
 
-hd kkcreate:page friends_list on friends
-hd kkcreate:request on friends
+hd kkcreate page:friends_list on friends
+hd kkcreate request on friends
 
 Global options:
 -V, --version               Print the hd version.
